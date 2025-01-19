d83069014206d3e588359f1e2af2626047f8b6af
subhrajit1990 <subhrajitsahu1990@gmail.com>
2025-01-19 18:06:39 +0530

    Updates

---
+++ precache-manifest.2e077b83838ea46bb45331d78d4b26f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5227836f8343ca733081c68d01894060",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "d8411534d0ce1770ca70",
+    "url": "/profile/static/css/main.a4783c72.chunk.css"
+  },
+  {
+    "revision": "9548af9dfeb4a4216b1f",
+    "url": "/profile/static/js/2.0210512e.chunk.js"
+  },
+  {
+    "revision": "d5a2140746d4a4e5c89e",
+    "url": "/profile/static/js/3.e13311ee.chunk.js"
+  },
+  {
+    "revision": "d8411534d0ce1770ca70",
+    "url": "/profile/static/js/main.a73fd081.chunk.js"
+  },
+  {
+    "revision": "e895bbf629c9f5d4afb6",
+    "url": "/profile/static/js/runtime~main.1e6147d2.js"
+  }
+]);
