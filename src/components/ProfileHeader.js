7fa5d2c56ee50030c3309ac8760be4a7592dcebe
subhrajit1990 <subhrajitsahu1990@gmail.com>
2026-01-04 08:31:05 +0530

    changes

--- src/components/ProfileHeader.js
+++ src/components/ProfileHeader.js
@@ -24,8 +24,7 @@ const ProfileHeader = ({ profileHeader }) => {
         <header className="profile-header" role="banner">
             <div className="profile-header-top" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
                 <div>
-                    <h1>{profileHeader.name}</h1>
-                    <h2>{profileHeader.designation}</h2>
+                    {/* left column reserved for name ribbon via CSS positioning */}
                 </div>
 
                 <div style={{ textAlign: 'right' }}>
@@ -36,6 +35,12 @@ const ProfileHeader = ({ profileHeader }) => {
                     </div>
                 </div>
             </div>
+
+            <div className="name-ribbon">
+                <div className="ribbon">{profileHeader.name}</div>
+                <div className="designation">{profileHeader.designation}</div>
+            </div>
+
             <hr className="designationLine" />
         </header>
     )
